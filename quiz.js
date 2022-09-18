5038ea0dbeed6a8bad5da4e2d7bae56c96e83ab5
aadarshhardiya <aadarsh.hardiya@gmail.com>
2022-09-18 23:19:08 +0530

    add some questions

--- quiz.js
+++ quiz.js
@@ -2,14 +2,16 @@ const quizForm = document.querySelector(".quiz-form")
 const submitAnsBtn = document.querySelector("#submit-ans-btn")
 const outputEl = document.querySelector("#output");
 
-const correctAnswers = ["90°" ,"Right angle" ,"Isosceles Triangle" ,"Equilateral Triangle" ,"Equilateral Triangle"];
+const correctAnswers = ["90°" ,"Right angle" ,"Isosceles Triangle" ,"Equilateral Triangle" ,"Equilateral Triangle" ,"75cm²" ,"Obtuse Angle Triangle" ,"Acute Angle Triangle" ,"35 cm2" ,"75°"];
 
 function calculateScore() {
     let score = 0;
     let index = 0; 
     const formResults = new FormData(quizForm);
     for (let value of formResults.values()){
+        console.log('index value ',index)
         if(value === correctAnswers[index]){
+            console.log('is correct ', index+1)
             score = score + 1;
         }
         index = index + 1;
